09a2a1a6427725c31bfbfb65c68ee709dc5e7f3b
Marti Raudsepp <marti@juffo.org>
2019-04-01 02:16:06 +0300

    Implemented proper error handling

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,43 @@ use std::ffi::CString;
 use std::net::IpAddr;
 use std::str::FromStr;
 
-use maxminddb::geoip2;
+use maxminddb::{geoip2, MaxMindDBError};
 use maxminddb::MaxMindDBError::AddressNotFoundError;
-use pg_extend::pg_magic;
+use pg_extend::{pg_magic, pg_error};
 use pg_extern_attr::pg_extern;
+use std::error::Error;
 
 /// This tells Postgres this library is a Postgres extension
 pg_magic!(version: pg_sys::PG_VERSION_NUM);
 
-fn geoip_country_internal(value: CString) -> Option<CString> {
-    let geoip = maxminddb::Reader::open_readfile("/usr/share/GeoIP/GeoLite2-Country.mmdb").unwrap();
+fn geoip_country_internal(value: CString) -> Result<Option<CString>, Box<Error>> {
+    let geoip = maxminddb::Reader::open_readfile("/usr/share/GeoIP/GeoLite2-Country.mmdb")?;
+    let ip: IpAddr = FromStr::from_str(value.to_str()?)?;
 
-    let ip: IpAddr = FromStr::from_str(value.to_str().unwrap()).unwrap();
-    println!("IP: {:?}", ip);
-    let ret: geoip2::Country = match geoip.lookup(ip) {
-        Ok(ret) => ret,
-        Err(AddressNotFoundError(_e)) => return None,
-        Err(e) => panic!(e)
-    };
-
-    Some(CString::new(ret.country?.iso_code?).unwrap())
+    let result: Result<geoip2::Country, MaxMindDBError> = geoip.lookup(ip);
+    match result {
+        Ok(ret) => Ok(Some(CString::new(ret.country.unwrap().iso_code.unwrap())?)),
+        Err(AddressNotFoundError(_e)) => Ok(None),
+        Err(e) => Err(e.into())
+    }
 }
 
 #[pg_extern]
 fn geoip_country(value: CString) -> CString {
     match geoip_country_internal(value)
     {
-        Some(cc) => cc,
-        None => CString::new("Error").unwrap()
+        Ok(Some(result)) => result,
+        Ok(None) => CString::new("N/A").unwrap(), // FIXME return SQL NULL here
+        Err(e) => {
+            pg_error::log(
+                pg_error::Level::Error,
+                file!(),
+                line!(),
+                module_path!(),
+                e.description()
+            );
+            return CString::new("N/A").unwrap()
+        }
     }
 }
 
@@ -42,9 +51,9 @@ mod tests {
 
     #[test]
     fn test_country() {
-        assert_eq!(geoip_country(CString::new("1.2.3.4").unwrap()),
-                   CString::new("US").unwrap());
-        assert_eq!(geoip_country(CString::new("127.0.0.1").unwrap()),
-                   CString::new("Error").unwrap()); // FIXME hack.
+        assert_eq!(geoip_country_internal(CString::new("8.8.8.8").unwrap()).unwrap(),
+                   Some(CString::new("US").unwrap()));
+        assert_eq!(geoip_country_internal(CString::new("127.0.0.1").unwrap()).unwrap(),
+                   None);
     }
 }
