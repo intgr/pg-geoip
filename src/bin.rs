20fcf6b039f630e542bd5371af36b1d94a4493e7
Marti Raudsepp <marti@juffo.org>
2019-04-01 03:06:40 +0300

    Now caching the GeoIP maxminddb instance per session

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,5 @@
+// #[macro_use] extern crate lazy_static;
+
 extern crate pg_extend;
 
 use pg_extend::pg_create_stmt_bin;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate lazy_static;
 extern crate pg_extend;
 extern crate pg_extern_attr;
 
@@ -10,15 +11,40 @@ use maxminddb::MaxMindDBError::AddressNotFoundError;
 use pg_extend::{pg_magic, pg_error};
 use pg_extern_attr::pg_extern;
 use std::error::Error;
+use std::sync::{Arc, Mutex};
+
+const DEFAULT_DB_PATH: &str = "/usr/share/GeoIP/GeoLite2-Country.mmdb";
+
+/// Cache the database instance on first open
+struct DatabaseCache {
+    db: Option<Arc<maxminddb::Reader<Vec<u8>>>>
+}
+
+impl DatabaseCache {
+    fn new() -> DatabaseCache {
+        DatabaseCache { db: None }
+    }
+
+    fn get(&mut self) -> Result<Arc<maxminddb::Reader<Vec<u8>>>, MaxMindDBError> {
+        if let None = self.db {
+            self.db = Some(Arc::new(maxminddb::Reader::open_readfile(DEFAULT_DB_PATH)?));
+        }
+        Ok(self.db.as_ref().unwrap().clone())
+    }
+}
+
+lazy_static::lazy_static! {
+    static ref DB_MANAGER: Mutex<DatabaseCache> = Mutex::new(DatabaseCache::new());
+}
 
 /// This tells Postgres this library is a Postgres extension
 pg_magic!(version: pg_sys::PG_VERSION_NUM);
 
 fn geoip_country_internal(value: CString) -> Result<Option<CString>, Box<Error>> {
-    let geoip = maxminddb::Reader::open_readfile("/usr/share/GeoIP/GeoLite2-Country.mmdb")?;
     let ip: IpAddr = FromStr::from_str(value.to_str()?)?;
+    let geoip_db = DB_MANAGER.lock().unwrap().get()?;
 
-    let result: Result<geoip2::Country, MaxMindDBError> = geoip.lookup(ip);
+    let result: Result<geoip2::Country, MaxMindDBError> = geoip_db.lookup(ip);
     match result {
         Ok(ret) => Ok(Some(CString::new(ret.country.unwrap().iso_code.unwrap())?)),
         Err(AddressNotFoundError(_e)) => Ok(None),
